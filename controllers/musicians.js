c12cefc36c9ee85b42cb375defc29b99a8734506
Nikhil Madhav <Nikhil.Madhav@sabre.com>
2015-09-16 16:58:33 -0500

    models

--- controllers/musicians.js
+++ controllers/musicians.js
@@ -1,8 +1,17 @@
 exports.findAll = function(req, res){
-  res.send([{
-    "id": 1,
-    "name": "Max",
-    "band": "Maximum Pain",
-    "instrument": "guitar"
-  }]);
+  Musician.find({},function(err, results) {
+    return res.send(results);
+  });
+};
+
+exports.import = function(req, res){
+  Musician.create(
+    { "name": "Ben", "band": "DJ Code Red", "instrument": "Reason" },
+    { "name": "Mike D.","band": "Kingston Kats", "instrument": "drums" },
+    { "name": "Eric", "band": "Eric", "instrument": "piano" },
+    { "name": "Paul", "band": "The Eyeliner", "instrument": "guitar" }
+  , function (err) {
+    if (err) return console.log(err);
+    return res.send(202);
+  });
 };
--- routes.js
+++ routes.js
@@ -1,4 +1,5 @@
 module.exports = function(app){
     var musicians = require('./controllers/musicians');
     app.get('/musicians', musicians.findAll);
+    app.get('/import', musicians.import);
 }

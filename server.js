5da6ca1800f803f2b27854ed3a7fec65e7e76670
Nikhil Madhav <Nikhil.Madhav@sabre.com>
2015-12-08 18:01:57 -0600

    change from https to htpp

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ var express = require('express'),
 //});
 //app.set("jsonp callback", true);
 //
-//var privateKey  = fs.readFileSync('ssl/server.key', 'utf8');
-//var certificate = fs.readFileSync('ssl/server.crt', 'utf8');
-//var credentials = {key: privateKey, cert: certificate};
+var privateKey  = fs.readFileSync('ssl/server.key', 'utf8');
+var certificate = fs.readFileSync('ssl/server.crt', 'utf8');
+var credentials = {key: privateKey, cert: certificate};
 //
 //var allowCrossDomain = function(req, res, next) {
 //  res.header('Access-Control-Allow-Origin', '*');

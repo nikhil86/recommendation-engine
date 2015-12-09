83ffd1839a0cba1b508d74cc9f9d9a61653483e0
Nikhil Madhav <Nikhil.Madhav@sabre.com>
2015-12-08 18:18:48 -0600

    change from https to htpp

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ var express = require('express'),
 //});
 //app.set("jsonp callback", true);
 //
-//var privateKey  = fs.readFileSync('ssl/server.key', 'utf8');
+var privateKey  = fs.readFileSync('ssl/server.key', 'utf8');
+console.log(privateKey);
 //var certificate = fs.readFileSync('ssl/server.crt', 'utf8');
 //var credentials = {key: privateKey, cert: certificate};
 //

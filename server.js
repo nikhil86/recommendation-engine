c47a9159d7728d7c059006b9c7db4263d736bb2c
Nikhil Madhav <Nikhil.Madhav@sabre.com>
2015-12-08 18:36:37 -0600

    change from https to htpp

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ var express = require('express'),
   https = require('https'),
   fs = require('fs'),
 	app = express(),
-  //bodyParser  = require("body-parser"),
-  mongoose = require('mongoose');
-  //L = require("./logger");
+  bodyParser  = require("body-parser"),
+  mongoose = require('mongoose'),
+  L = require("./logger");
 
 //var mongoUri = process.env.MONGOLAB_URI || 'mongodb://localhost:27017';
 var mongoUri = process.env.MONGOLAB_URI || 'mongodb://heroku_jlkph440:5egmvqi1lq42rj3jea099ck4kq@ds027799.mongolab.com:27799/heroku_jlkph440';
@@ -20,21 +20,21 @@ var privateKey  = fs.readFileSync('ssl/server.key', 'utf8');
 var certificate = fs.readFileSync('ssl/server.crt', 'utf8');
 var credentials = {key: privateKey, cert: certificate};
 //
-//var allowCrossDomain = function(req, res, next) {
-//  res.header('Access-Control-Allow-Origin', '*');
-//  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-//  res.header('Access-Control-Allow-Headers', 'Content-Type');
-//  next();
-//};
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+};
 //
-//app.use(bodyParser.json({ extended: true, parameterLimit: 10000, limit: 1024 * 1024 * 10 }));
-//// parse application/x-www-form-urlencoded
-//app.use(bodyParser.urlencoded({ extended: true, parameterLimit: 10000, limit: 1024 * 1024 * 10 }));
-//app.use(allowCrossDomain);
+app.use(bodyParser.json({ extended: true, parameterLimit: 10000, limit: 1024 * 1024 * 10 }));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true, parameterLimit: 10000, limit: 1024 * 1024 * 10 }));
+app.use(allowCrossDomain);
 //
-//require('./models/user');
-//require('./models/topsis');
-//require('./routes')(app);
+require('./models/user');
+require('./models/topsis');
+require('./routes')(app);
 
 app.get('/', function(req, res) {
   res.send('Hello Seattle\n');

2889055061233be7a5b8d8607a15b30ef9854a47
Nikhil Madhav <Nikhil.Madhav@sabre.com>
2016-04-13 21:01:52 +0530

    incorporating stops into the equation

--- models/topsis.js
+++ models/topsis.js
@@ -182,13 +182,13 @@ exports.scoreItins = function (pref, flights) {
               );
               flight.scores.topsis[i][k] += flight.scores.fareScore * preference.weights[k];
               break;
-            case "stops":
-              flight.scores.stopsScore = fuzzyMember(
-                  Math.log(parseFloat(flight.numberOfStops)),
-                  [0, 0, Math.log(flights.StopsRange.BestStops), Math.log(flights.StopsRange.WorstStops)]
-              );
-              flight.scores.stopsScore[i][k] += flight.scores.stopsScore * preference.weights[k];
-              break;
+            //case "stops":
+            //  flight.scores.stopsScore = fuzzyMember(
+            //      Math.log(parseFloat(flight.numberOfStops)),
+            //      [0, 0, Math.log(flights.StopsRange.BestStops), Math.log(flights.StopsRange.WorstStops)]
+            //  );
+            //  flight.scores.stopsScore[i][k] += flight.scores.stopsScore * preference.weights[k];
+            //  break;
             case "time":
               flight.scores.timeScore = fuzzyMember(
                 Math.log(flight.totalTripDuration),
